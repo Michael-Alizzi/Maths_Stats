190f3a4f9aeef3a2d05b1f2f3103a41baa199a5f
Michael-Alizzi <myeckal123@gmail.com>
2025-03-16 01:07:00 +1100

    new functions added to source file and added rproj file

--- Maths Functions.R
+++ Maths Functions.R
@@ -1,12 +1,12 @@
 # Toss a 6-sided die 3 times.
 # What is the number of outcomes in the sample space?
-sample_space_calc <- function(outcomes) {
+multi_count_calc <- function(outcomes) {
   
   return(prod(outcomes))  
   
 }
 
-attr(sample_space_calc, 'Multiplication Rule') <- "Calculates the product of all elements in a numeric vector. Used to determine total possible outcomes for k independent experiments, where each experiment has a specific number of outcomes. Input must be a numeric vector."
+attr(multi_count_calc, "Multiplication Rule in Counting") <- "Calculates the product of all elements in a numeric vector. Used to determine total possible outcomes for k independent experiments, where each experiment has a specific number of outcomes. Input must be a numeric vector."
 
 # A particular committee has four members (n = 4). One member must chair the committee, and 
 # a different committee member must take minutes from meetings (r = 2). How many different 
@@ -17,7 +17,7 @@ perm_calc = function(n, r){
   
   }
 
-attr(perm_calc, 'Permutation Rule') <- 'Determines the total number of ways to arrange r objects selected from n distinct objects in a specific order.'
+attr(perm_calc, "Permutation Rule") <- "Determines the total number of ways to arrange r objects selected from n distinct objects in a specific order."
 
 # From a committee of four people(n = 4), two committee members will need to 
 # present the committee's recommendations to the board of directors (r = 2).
@@ -29,7 +29,7 @@ combination_calc = function(n, r){
 
 }
 
-attr(combination_calc, 'Combination Rule') <- 'Calculates the number of ways to choose r objects from n distinct objects where order does not matter.'
+attr(combination_calc, "Combination Rule") <- "Calculates the number of ways to choose r objects from n distinct objects where order does not matter."
 
 # If there was only two parties in the US Senate, 
 # whats the probability of being a republican if 
@@ -41,21 +41,7 @@ complement_rule <- function(A) {
   return(Ac)
 }
 
-attr(complement_rule, 'Complement Rule') <- "Calculates the complement of P(A). i.e. P(Ac)."
-
-# The probability of being a republican or
-# female if: 
-# The probability of being female is 0.17 .
-# The probability of being a republican is 0.47.
-# The probability of both is 0.04.
-add_rule <- function(A, B, AandB) {
-  
-  AorB <- A+B-AandB 
-  
-  return(AorB)
-}
-
-attr(add_rule, 'Additive Rule') <- "Calculates the probability of P(A or B) when A and B are NOT disjoint."
+attr(complement_rule, "Complement Rule") <- "Calculates the complement of P(A). i.e. P(Ac)."
 
 # The probability of pulling a jack and a
 # seven from a standard deck of 52 cards.
@@ -79,20 +65,7 @@ disj_prob <- function(A, B, AorB) {
   
 }
 
-attr(disj_prob, 'Disjoint Check & Probability') <- "Determines if P(A) and P(B) are disjoint (mutually exclusive) and calculates P(A or B)."
-
-# The probability of being an athlete who
-# also wants an Olympic gold medal P(B) if: 
-# The probability of being an athlete is 0.37 P(A).
-# The probability of wanting an Olympic gold medal given someone is an athlete is 0.73 P(B|A).
-multi_rule <- function(A, BgivenA) {
-  
-  AandB <- A*BgivenA 
-  
-  return(AandB)
-}
-
-attr(multi_rule, 'Multiplicative Rule') <- "Calculates the probability of P(A & B) when A and B are NOT independent."
+attr(disj_prob, "Disjoint Check & Probability") <- "Determines if P(A) and P(B) are disjoint (mutually exclusive) and calculates P(A or B)."
 
 # The probability of being an athlete who
 # also wants an Olympic gold medal if: 
@@ -105,7 +78,7 @@ condit_prob <- function(A, AandB) {
   return(BgivenA)
 }
 
-attr(condit_prob, 'Conditional Probability') <- "Calculates the probability of P(B|A) when A and B are NOT independent."
+attr(condit_prob, "Conditional Probability") <- "Calculates the probability of P(B|A) when A and B are NOT independent."
 
 # The probability of rolling a four on a six sided die 
 # and rolling two dice and the sum being 7.
@@ -151,7 +124,7 @@ pair_indep_prob <- function(A = NA, B = NA, AandB = NA, AgivenB = NA) {
   
 }
 
-attr(pair_indep_prob, 'Pairwise Independence Check & Probability') <- "Determines whether P(A) and P(B) are independent using P(A & B) = P(A)P(B) or P(A|B) = P(A). Provide either: A, B, and AandB (to check P(A & B) = P(A)P(B)) OR A and AgivenB (to check P(A|B) = P(A))."
+attr(pair_indep_prob, "Pairwise Independence Check & Probability") <- "Determines whether P(A) and P(B) are independent using P(A & B) = P(A)P(B) or P(A|B) = P(A). Provide either: A, B, and AandB (to check P(A & B) = P(A)P(B)) OR A and AgivenB (to check P(A|B) = P(A))."
 
 # A coin is tossed twice.
 # A be the event 'head on the first toss' .
@@ -169,13 +142,13 @@ mutu_indep_prob <- function(single_event_prob_vec, multi_event_prob) {
     
     cat("Yes \n")
     
-    cat(sprintf("Probability of all events intersection: %s \n", expected_prob))
+    cat(sprintf("Probability of all event's intersection: %s \n", expected_prob))
     
   } else {
     
     cat("No \n")
     
-    cat(sprintf("Probability of all events intersection: %s \n", multi_event_prob))
+    cat(sprintf("Probability of all event's intersection: %s \n", multi_event_prob))
     
   }
   
@@ -183,6 +156,74 @@ mutu_indep_prob <- function(single_event_prob_vec, multi_event_prob) {
 }
 
 attr(mutu_indep_prob, 'Mutual Independence Check & Probability') <- "Determines whether any number of events are mutually independent. Include each event's individual probabilities as a numeric vector of length greater than 2 in the single_event_prob_vec parameter and the probability intersection of all events as a numeric vector of length 1 in the multi_event_prob parameter"
+
+# The probability of being an athlete who
+# also wants an Olympic gold medal P(B) if: 
+# The probability of being an athlete is 0.37 P(A).
+# The probability of wanting an Olympic gold medal given someone is an athlete is 0.73 P(B|A).
+multi_prob_calc <- function(A, BgivenA) {
+  
+  AandB <- A*BgivenA 
+  
+  return(AandB)
+}
+
+attr(multi_prob_calc, 'Multiplication Rule in Probability with two events') <- "Calculates the probability of P(A & B) when A and B are NOT independent."
+
+# The probability of being an athlete who
+# also wants an Olympic gold medal P(B) if: 
+# The probability of being an athlete is 0.37 P(A).
+# The probability of wanting an Olympic gold medal given someone is an athlete is 0.73 P(B|A).
+multi_event_prob_calc <- function(CgivenAandB, BgivenA, A) {
+  
+  AandBandC <- CgivenAandB*BgivenA*A
+  
+  return(AandBandC)
+}
+
+attr(multi_event_prob_calc, 'Multiplication Rule in Probability with More Than Two Events') <- "Calculates the probability of any number of events occuring together when the events are NOT independent. The CgivenAandB P(C|A&B) parameter is the probability that C occurs given A and B have already occurred. The BgivenA P(B|A) parameter is the probability that B occurs given A has already occured. The A P(A) parameter is the probability that A occurs"
+
+# The probability of being a republican or
+# female if: 
+# The probability of being female is 0.17 .
+# The probability of being a republican is 0.47.
+# The probability of both is 0.04.
+add_rule <- function(A, B, AandB) {
+  
+  AorB <- A+B-AandB 
+  
+  return(AorB)
+}
+
+attr(add_rule, 'Additive Rule') <- "Calculates the probability of P(A or B) when A and B are NOT disjoint."
+
+# Urn I contains 3 red balls and 4 white balls.
+# Urn II contains 2 red balls and 4 white balls.  
+# A ball is drawn from Urn I and placed unseen into Urn II.
+# A ball is now drawn at random from Urn II.
+# What is the probability that this second ball is red P(B)?
+law_of_prob = function(BgivenAi, Ai){
+  
+  B <- sum(BgivenAi*Ai)
+  
+  return(B)
+}
+
+attr(law_of_prob, "The Law of Total Probability") <- "Computes P(B) using the Law of Total Probability, given P(B|Ai) and P(Ai). The BgivenAi parameter must contain P(B|Ai) as a numeric vector, and the Ai parameter must contain the probabilities of P(Ai) as a numeric vector."
+
+# Urn I contains 3 red balls and 4 white balls.
+# Urn II contains 2 red balls and 4 white balls.  
+# A ball is drawn from Urn I and placed unseen into Urn II.
+# A ball is now drawn at random from Urn II.
+# Given that the second ball drawn is red P(B), what is the probability that the first ball was white P(Aj|B)?
+bayes_funct = function(BgivenAj, Aj, B){
+  
+  AjgivenB <- (BgivenAj*Aj)/B
+  
+  return(AjgivenB)
+}
+
+attr(bayes_funct, "Bayes' Theorem") <- "Computes P(Aj|B) using Bayes' Theorem, given P(B|Aj), P(Aj), and P(B). The BgivenAj parameter must contain the probability of B given Aj (P(B|Aj)) as a numeric vector, Aj must contain the probability of Aj as a numeric vector, and B must be the probability of B as a numeric value."
 
 # A charitable organization is running a rafﬂe as a fundraiser. 
 # They offer a grand prize of $500, two second prizes of $100, and ten third prizes of $20 
@@ -204,7 +245,7 @@ disc_rand_funct = function(outcome, probability){
               StandardDeviation = disc_rand_std_value))
 }
 
-attr(disc_rand_funct, 'Mean, variance and standard deviation of a discrete Random Variable') <- 'The output and probability parameters must be numeric vectors.'
+attr(disc_rand_funct, "Mean, variance and standard deviation of a discrete Random Variable") <- "The output and probability parameters must be numeric vectors."
 
 # A gambler places 4 bets (n = 4) on black in roulette, 
 # where the probability of winning a single bet is 18/37 (p).
@@ -222,7 +263,7 @@ bin_prob_k = function(n, p, k){
   
 }
 
-attr(bin_prob_k, 'Binomial Probability of k') <- 'Computes the probability of getting exactly k successes in n events, given a success probability p.'
+attr(bin_prob_k, "Binomial Probability of k") <- "Computes the probability of getting exactly k successes in n events, given a success probability p."
 
 # A gambler places 4 bets (n = 4) on black in roulette, 
 # where the probability of winning a single bet is 18/37 (p).
@@ -273,11 +314,11 @@ bin_rand_funct = function(n, p){
               StandardDeviation = disc_rand_std_value))
 }
 
-attr(disc_rand_funct, 'Mean, variance and standard deviation of a binomial Random Variable') <- ' Both the n (the number of trials) and p (probability of success) parameters must be numeric vectors of length 1.'
+attr(disc_rand_funct, "Mean, variance and standard deviation of a binomial Random Variable") <- " Both the n (the number of trials) and p (probability of success) parameters must be numeric vectors of length 1."
 
 std_from_mean = function(std, mean, x){
   (mean-x)/std
 }
 
-attr(disc_rand_funct, 'How many standard deviations away from the mean is x') <- 'std = Standard Deviation, mean, x = how many standard deviations away from the mean is x'
+attr(disc_rand_funct, "How many standard deviations away from the mean is x") <- "std = Standard Deviation, mean, x = how many standard deviations away from the mean is x"
 
